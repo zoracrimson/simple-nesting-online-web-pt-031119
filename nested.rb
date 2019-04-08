21663530445e68340db2bbf6d9e3ece371a76e0a
Kat Maldon <katharinamaldon@gmail.com>
2019-04-08 17:37:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+programmer_hash
+
 end
