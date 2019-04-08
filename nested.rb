ea395d7e879f9f05d690a4d66f32b4aad0c26675
Kat Maldon <katharinamaldon@gmail.com>
2019-04-08 17:38:37 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,15 +59,12 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -77,6 +78,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
 
@@ -99,9 +104,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
+programmer_hash[:alan_kay][:known_for]  = "GUI"
+   
+programmer_hash
      
 end
 
@@ -121,5 +127,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+programmer_hash
 
 end
